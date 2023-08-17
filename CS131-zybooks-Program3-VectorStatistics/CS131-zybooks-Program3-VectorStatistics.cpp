c90cd22e1479f0a239376ec0d9515ef23386137c
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-08-17 12:06:04 -0700

    Spaced out comments.

--- CS131-zybooks-Program3-VectorStatistics/CS131-zybooks-Program3-VectorStatistics.cpp
+++ CS131-zybooks-Program3-VectorStatistics/CS131-zybooks-Program3-VectorStatistics.cpp
@@ -59,13 +59,18 @@ int main()
 
 
 /*
-Program Specifications Write a program to calculate the minimum, maximum, mean, median, mode, and whether a vector is a palindrome.
+Program Specifications Write a program to calculate 
+the minimum, maximum, mean, median, mode, and whether a vector is a palindrome.
 
-Note: This program is designed for incremental development. Complete each step and submit for grading before starting the next step. Only a portion of tests pass after each step but confirm progress.
+Note: This program is designed for incremental development. 
+Complete each step and submit for grading before starting the next step. 
+Only a portion of tests pass after each step but confirm progress.
 
-Step 0. Review the starter code in main(). A vector is filled with integers from standard input. The first value indicates how many numbers are to follow and be placed in the vector.
+Step 0. Review the starter code in main(). A vector is filled with integers from standard input. 
+The first value indicates how many numbers are to follow and be placed in the vector.
 
-Step 1 (2 pts). Use a loop to process each vector element and output the minimum and maximum values. Submit for grading to confirm one test passes.
+Step 1 (2 pts). Use a loop to process each vector element and output the minimum and maximum values. 
+Submit for grading to confirm one test passes.
 
 Ex: If input is:
 
@@ -74,7 +79,11 @@ the output is:
 
 Minimum: 1
 Maximum: 99
-Step 2 (2 pts). Use a loop to sum all vector elements and calculate the mean (or average). Output the mean with one decimal place using cout << fixed << setprecision(1); once before all other cout statements. Submit for grading to confirm two tests pass.
+
+Step 2 (2 pts). Use a loop to sum all vector elements and calculate the mean (or average). 
+Output the mean with one decimal place using cout << fixed << setprecision(1); 
+once before all other cout statements. 
+Submit for grading to confirm two tests pass.
 
 Ex: If input is:
 
@@ -84,7 +93,10 @@ the output is:
 Minimum: 1
 Maximum: 99
 Mean: 21.7
-Step 3 (2 pts). Use a loop to determine if the vector is a palindrome, meaning values are the same from front to back and back to front. Output "true" or "false". Submit for grading to confirm three tests pass.
+
+Step 3 (2 pts). Use a loop to determine if the vector is a palindrome, 
+meaning values are the same from front to back and back to front. Output "true" or "false". 
+Submit for grading to confirm three tests pass.
 
 Ex: If input is:
 
@@ -95,6 +107,7 @@ Minimum: 1
 Maximum: 5
 Mean: 2.8
 Palindrome: true
+
 Step 4 (1 pt). main() includes a call to sort(), which sorts the vector elements into ascending order. Do not sort the vector before step 4. After sorting, identify the median. The median is located in the middle of the vector if the vector's size is odd. Otherwise, the median is the average of the middle two values. Output the median with one decimal place. Submit for grading to confirm four tests pass.
 
 Ex: If input is:
@@ -107,6 +120,7 @@ Maximum: 7
 Mean: 4.8
 Palindrome: false
 Median: 5.5
+
 Step 5 (3 pts). Challenging! Identify the mode after the vector is sorted in ascending order. The mode is the value that appears most frequently. Assume only one mode exists. Hint: Use a loop to process each vector element, looking for the longest sequence of identical values. Submit for grading to confirm all tests pass.
 
 Ex: If input is:
